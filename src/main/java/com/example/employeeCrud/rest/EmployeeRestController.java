313fac104eb2e150270e26d18a5376931dd9cba2
Ali Majidi <amajidi1382@gmail.com>
2025-05-18 21:26:57 +0330

    feat: create rest controller object to access dao

---
+++ src/main/java/com/example/employeeCrud/rest/EmployeeRestController.java
@@ -0,0 +1,28 @@
+package com.example.employeeCrud.rest;
+
+import com.example.employeeCrud.dao.EmployeeDAO;
+import com.example.employeeCrud.enitity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+	private EmployeeDAO employeeDAO;
+
+	@Autowired
+	public EmployeeRestController(EmployeeDAO employeeDAO) {
+		this.employeeDAO = employeeDAO;
+	}
+
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees() {
+		return employeeDAO.findAll();
+	}
+
+}
