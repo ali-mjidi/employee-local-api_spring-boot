fd39d5f8a6449065b5dd8a0cb6f2fa23eecab71f
Ali Majidi <amajidi1382@gmail.com>
2025-05-19 03:26:43 +0330

    feat: replace manual REST controllers with Spring Data REST

--- src/main/java/com/example/employeeCrud/rest/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeeCrud.rest;
-
-public class EmployeeNotFoundException extends RuntimeException {
-	public EmployeeNotFoundException(int id) {
-		super("Employee id not found - " + id);
-	}
-}
