b3c49ca0df11e4361694d08887e5a8251c58193c
Ali Majidi <amajidi1382@gmail.com>
2025-05-18 23:50:41 +0330

    feat: use dao methods in service layer

--- src/main/java/com/example/employeeCrud/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/employeeCrud/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.employeeCrud.dao.EmployeeDAO;
 import com.example.employeeCrud.enitity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,13 +13,33 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 	private EmployeeDAO employeeDAO;
 
+	// Constructor
 	@Autowired
 	public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
 		this.employeeDAO = employeeDAO;
 	}
 
+	// Implemented Methods
 	@Override
 	public List<Employee> findAll() {
 		return employeeDAO.findAll();
 	}
+
+	@Override
+	public Employee findById(int id) {
+		return employeeDAO.findById(id);
+	}
+
+	@Override
+	@Transactional
+	public Employee save(Employee theEmployee) {
+		return employeeDAO.save(theEmployee);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int id) {
+		employeeDAO.deleteById(id);
+	}
+
 }
